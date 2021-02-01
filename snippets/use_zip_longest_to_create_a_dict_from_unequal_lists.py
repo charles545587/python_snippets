d133a8fb40d74cda18eb4660a3088b9abe9aa050
CharlesRoberts <achrober@.amazon.co.uk>
2021-02-01 10:50:36 +0000

    added using zip_longest to create a dict

---
+++ snippets/use_zip_longest_to_create_a_dict_from_unequal_lists.py
@@ -0,0 +1,11 @@
+# create a dictionary from two unequal lists using zip_longest
+# and dict
+import itertools
+
+my_short_list = ['oranges', 'carrots', 'peas']
+my_long_list = ['first_item', 'second_item', 'third_item', 'fourth_item']
+
+dictionary_from_zip_longest = dict(itertools.zip_longest(my_long_list, my_short_list, fillvalue=None))
+print(dictionary_from_zip_longest)
+# Response
+# {'first_item': 'oranges', 'second_item': 'carrots', 'third_item': 'peas', 'fourth_item': None}
