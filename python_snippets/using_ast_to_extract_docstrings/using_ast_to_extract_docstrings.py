17f82fcf8af601d794259edb6bb6777a7caddfd6
CharlesRoberts <achrober@.amazon.co.uk>
2021-02-23 08:27:21 +0000

    updated version contributed by Jud Neer

--- python_snippets/using_ast_to_extract_docstrings/using_ast_to_extract_docstrings.py
+++ python_snippets/using_ast_to_extract_docstrings/using_ast_to_extract_docstrings.py
@@ -1,42 +1,44 @@
-# Using AST to extract docstrings from python.
-
-# create a separate file called example_function.py
-# create a function with a docstring in example_function.py
-# for example:
-# def my_example_function():
-#     """
-#     This is an example docstring
-#     """
-import os
 import ast
-import json
 
-directory = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(directory, "example_function.py")
 
-with open(file_path, 'r') as f:
-    file_contents = f.read()
+def load_source_code(source_filename=__file__):
+    """
+    Open a script to parse.
+​
+    :param source_filename: Full path to script to parse, if not provided,
+                            this script will parse itself.
+    :return: Script content as a string
+    """
+    return open(source_filename, 'r').read()
 
-# ast will parse a string, confirm that the opened file is a string
-print(type(file_contents))
 
-# parse the file using ast
-parsed_file = ast.parse(file_contents)
+def get_function_list(source_code):
+    """
+    Parse the source code using AST and get a list of function objects.
+​
+    :param source_code: Python source to parse
+    :return: List of function objects from source
+    """
+    source_tree = ast.parse(source_code)
+    return (i for i in source_tree.body if isinstance(i, ast.FunctionDef))
 
-# extract the functions from the parsed file using ast.FunctionDef
-# using a list comprehension
-list_of_function_definitions = (
-    [function for function in parsed_file.body
-        if isinstance(function, ast.FunctionDef)])
 
-# Generate a list of the function names using a list comprehension
-function_names = [function.name for function in list_of_function_definitions]
+def get_function_details(functions):
+    """
+    Create a generator for function name/docstring tuples.
+​
+    :param functions: Iterable of function objects from source code
+    :return: Generator of tuples of fuction names and docstrings
+    """
+    return ((f.name, ast.get_docstring(f)) for f in functions)
 
-# Use the ast.get_docstring on an ast.FunctionDef object
-for function in list_of_function_definitions:
-    function_name = function.name
-    docstring = ast.get_docstring(function)
 
-    print(function_name + ": " +  docstring)
+source_code = load_source_code()
+functions = get_function_list(source_code)
+function_details = get_function_details(functions)
 
-# With lots of help from https://gabrielelanaro.github.io/blog/2014/12/12/extract-docstrings.html
+for name, docstring in function_details:
+    print(name)
+    print('-' * len(name))
+    print(docstring)
+    print()
