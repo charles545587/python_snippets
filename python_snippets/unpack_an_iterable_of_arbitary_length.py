8b884c0587b89b5a725f5cbaf369af8e94ca1fdb
Charles Roberts <achrober@amazon.com>
2021-11-29 20:28:37 +0000

    removed a spelling mistake

--- python_snippets/unpack_an_iterable_of_arbitary_length.py
+++ python_snippets/unpack_an_iterable_of_arbitary_length.py
@@ -1,15 +1,15 @@
 # Unpack an iterable of arbitary length using 'star expressions'
 
-my_shoppling = ("fruit", "apples", "bananas", "cherries")
+my_shopping = ("fruit", "apples", "bananas", "cherries")
 
 # Use the star expression to unpack the elements
-category, *list_of_fruits = my_shoppling
+category, *list_of_fruits = my_shopping
 print(category, list_of_fruits)
 # Returns fruit ['apples', 'bananas', 'cherries']
 # The star expression creates a list from the unpacked elements
 
 # Create a dictionary from the returned objects
-my_shopping = dict({category: list_of_fruits})
-print(my_shopping)
+my_shopping_dict = dict({category: list_of_fruits})
+print(my_shopping_dict)
 # Returns {'fruit': ['apples', 'bananas', 'cherries']}
 
